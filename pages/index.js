c7b2fc70124477f3e7869dbfebd78596ba0b6338
Joseph Ogbu <fallyfox@gmail.com>
2024-03-01 17:39:29 +0100

    testing git branches

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@
 export default function Index() {
   return (
     <>
-      <p>hello</p>
-      <p>world</p>
+      <main>
+        <div>
+          <h1>Learning about GIT branches</h1>
+        </div>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laudantium optio vero in consectetur cumque ut facere temporibus quasi qui!</p>
+      </main>
     </>
   );
 }
