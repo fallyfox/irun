c627447db43f026e529b3d2db600e5b168330d5c
Joseph Ogbu <fallyfox@gmail.com>
2024-03-09 10:42:28 +0100

    hero section build

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
+import Link from "next/link";
+import Head from "next/head";
+import { Merriweather } from "next/font/google";
+
+const MerriReg400 = Merriweather({
+  subsets:['latin'],
+  display:'swap',
+  weight:'400'
+});
+
+const MerriReg900 = Merriweather({
+  subsets:['latin'],
+  display:'swap',
+  weight:'900'
+});
 
 export default function Index() {
   return (
     <>
-      <main>
-        <div>
-          <h1>Learning about GIT branches</h1>
+      <Head>
+        <title>Irun | Shop for hairs with fixes</title>
+      </Head>
+
+      <main className="h-screen">
+        <div className="h-full w-full hidden lg:flex justify-center items-center bg-desktop">
+          <div className="h-full w-full bg-[#402B3A]/50 flex justify-center items-center">
+            <div className="w-full md:w-[520px] flex flex-col items-center gap-6 px-4 md:px-0">
+              <h1 className={`${MerriReg900.className} text-4xl lg:text-6xl text-center text-[#F8F4EC]`}>Shop for hairs and book an appoint for a fix</h1>
+              <Link href='/auth/signup' className="h-[48px] w-full md:w-[300px] flex justify-center items-center bg-[#402B3A] text-[#F8F4EC] text-2xl px-4 rounded-md">Get Started</Link>
+            </div>
+          </div>
         </div>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laudantium optio vero in consectetur cumque ut facere temporibus quasi qui!</p>
+
+        <div className="h-full w-full lg:hidden bg-mobile">
+
+        </div>
       </main>
     </>
   );
